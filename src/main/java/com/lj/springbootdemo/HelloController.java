57203fa0deebecd4eea03ab4815c398c5584c3ff
刘军 <jun.liu@weimob.com>
2018-06-20 10:54:18 +0800

    Initial commit

---
+++ src/main/java/com/lj/springbootdemo/HelloController.java
@@ -0,0 +1,22 @@
+package com.lj.springbootdemo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author liujun
+ * @date 2018/6/19 下午7:50
+ */
+
+@RestController
+public class HelloController {
+
+    @Value("${name}")
+    private String name;
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello Spring Boot!" + "-" + name;
+    }
+}
